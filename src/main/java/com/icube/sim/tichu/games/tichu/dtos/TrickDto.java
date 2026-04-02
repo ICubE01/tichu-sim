9eebde002f1cd01bf65070eadbd407b8cf1c5648
ICubE- <jehoyeon01@gmail.com>
2026-04-02 21:05:46 +0900

    Add fields in `TrickDto`

--- src/main/java/com/icube/sim/tichu/games/tichu/dtos/TrickDto.java
+++ src/main/java/com/icube/sim/tichu/games/tichu/dtos/TrickDto.java
@@ -1,8 +1,17 @@
 package com.icube.sim.tichu.games.tichu.dtos;
 
 import com.icube.sim.tichu.games.tichu.tricks.TrickType;
+import org.jspecify.annotations.Nullable;
 
 import java.util.List;
 
-public record TrickDto(int playerIndex, TrickType type, List<CardDto> cards) {
+public record TrickDto(
+        int playerIndex,
+        TrickType type,
+        List<CardDto> cards,
+        @Nullable Float rank,
+        @Nullable Integer minRank,
+        @Nullable Integer maxRank,
+        @Nullable Float phoenixRank
+) {
 }
