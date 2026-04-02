f8dfb72b4bf6ad00acf61f0ff9b759ca4dce8f4a
ICubE- <jehoyeon01@gmail.com>
2026-04-02 23:33:39 +0900

    Update `TrickMapper`

--- src/main/java/com/icube/sim/tichu/games/tichu/dtos/TrickDto.java
+++ src/main/java/com/icube/sim/tichu/games/tichu/dtos/TrickDto.java
@@ -1,10 +1,12 @@
 package com.icube.sim.tichu.games.tichu.dtos;
 
 import com.icube.sim.tichu.games.tichu.tricks.TrickType;
+import lombok.Builder;
 import org.jspecify.annotations.Nullable;
 
 import java.util.List;
 
+@Builder
 public record TrickDto(
         int playerIndex,
         TrickType type,
--- src/main/java/com/icube/sim/tichu/games/tichu/mappers/TrickMapper.java
+++ src/main/java/com/icube/sim/tichu/games/tichu/mappers/TrickMapper.java
@@ -1,7 +1,7 @@
 package com.icube.sim.tichu.games.tichu.mappers;
 
 import com.icube.sim.tichu.games.tichu.dtos.TrickDto;
-import com.icube.sim.tichu.games.tichu.tricks.Trick;
+import com.icube.sim.tichu.games.tichu.tricks.*;
 
 import java.util.List;
 
@@ -9,7 +9,52 @@ public class TrickMapper {
     private final CardMapper cardMapper = new CardMapper();
 
     public TrickDto toDto(Trick trick) {
-        return new TrickDto(trick.getPlayerIndex(), trick.getType(), cardMapper.toDtos(trick.getCards()));
+        var builder = TrickDto.builder()
+                .playerIndex(trick.getPlayerIndex())
+                .type(trick.getType())
+                .cards(cardMapper.toDtos(trick.getCards()));
+
+        switch (trick) {
+            case SingleTrick singleTrick -> {
+                var rank = singleTrick.getRank();
+                builder.rank(rank).phoenixRank(singleTrick.isPhoenixUsed() ? rank : null);
+            }
+            case PairTrick pairTrick -> {
+                var rank = (float) pairTrick.getRank();
+                builder.rank(rank).phoenixRank(pairTrick.isPhoenixUsed() ? rank : null);
+            }
+            case ThreeOfAKindTrick threeOfAKindTrick -> {
+                var rank = (float) threeOfAKindTrick.getRank();
+                builder.rank(rank).phoenixRank(threeOfAKindTrick.isPhoenixUsed() ? rank : null);
+            }
+            case FullHouseTrick fullHouseTrick -> {
+                var rank = (float) fullHouseTrick.getRank();
+                var phoenixRank = fullHouseTrick.getPhoenixRank();
+                builder.rank(rank).phoenixRank(phoenixRank == null ? null : Float.valueOf(phoenixRank));
+            }
+            case ConsecutivePairsTrick consecutivePairsTrick -> {
+                var phoenixRank = consecutivePairsTrick.getPhoenixRank();
+                builder.minRank(consecutivePairsTrick.getMinRank())
+                        .maxRank(consecutivePairsTrick.getMaxRank())
+                        .phoenixRank(phoenixRank == null ? null : Float.valueOf(phoenixRank));
+            }
+            case StraightTrick straightTrick -> {
+                var phoenixRank = straightTrick.getPhoenixRank();
+                builder.minRank(straightTrick.getMinRank())
+                        .maxRank(straightTrick.getMaxRank())
+                        .phoenixRank(phoenixRank == null ? null : Float.valueOf(phoenixRank));
+            }
+            case DogTrick ignored -> {}
+            case FourOfAKindTrick fourOfAKindTrick -> {
+                builder.rank((float) fourOfAKindTrick.getRank());
+            }
+            case StraightFlushTrick straightFlushTrick -> {
+                builder.minRank(straightFlushTrick.getMinRank()).maxRank(straightFlushTrick.getMaxRank());
+            }
+            case null, default -> throw new IllegalArgumentException("Invalid trick type");
+        }
+
+        return builder.build();
     }
 
     public List<TrickDto> toDtos(List<Trick> tricks) {
