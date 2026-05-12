7a95081e1be7d2edcc4f1a825844476aad742277
ICubE- <jehoyeon01@gmail.com>
2026-05-13 00:03:03 +0900

    Enable scheduling

--- src/main/java/com/icube/sim/tichu/TichuSimApplication.java
+++ src/main/java/com/icube/sim/tichu/TichuSimApplication.java
@@ -2,8 +2,10 @@ package com.icube.sim.tichu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TichuSimApplication {
     public static void main(String[] args) {
         SpringApplication.run(TichuSimApplication.class, args);
